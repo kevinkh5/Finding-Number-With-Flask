3787d614985326b6ac5e9519c1228b82f430c21d
kevin <kevinkyon5@yahoo.com>
2024-01-06 18:39:23 +0900

    flask route

--- hello.py
+++ hello.py
@@ -5,3 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/bye')
+def bye():
+    return 'Bye !'
+
+@app.route('/username/<name>/<int:number>')
+def greet(name, number):
+    return f'Hello there! {name} , you are {number}years old!'
+
+
+if __name__  == "__main__":
+    app.run(debug=True)
+
+
