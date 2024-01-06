6651213c67e3b06a21eadde7c1233ab60c5febd2
kevin <kevinkyon5@yahoo.com>
2024-01-06 19:08:59 +0900

    added decorator

--- hello.py
+++ hello.py
@@ -1,6 +1,23 @@
 from flask import Flask
 
 app = Flask(__name__)
+def make_bold(function):
+    def wrapper_function():
+        string = function()
+        return '<b>'+string+'</b>'
+    return wrapper_function
+
+def make_emphasis(function):
+    def wrapper_function():
+        string = function()
+        return '<em>'+string+'</em>'
+    return wrapper_function
+
+def make_underlined(function):
+    def wrapper_function():
+        string = function()
+        return '<u>'+string+'</u>'
+    return wrapper_function
 
 @app.route('/')
 def hello_world():
@@ -9,6 +26,9 @@ def hello_world():
            '<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbndkYmI4aGFhbDh6bDVmNmsxazRrN3ZhdmI3dHg5d2V5YnZjdmJxcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YRVP7mapl24G6RNkwJ/giphy.gif" width=200>'
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return 'Bye !'
 
